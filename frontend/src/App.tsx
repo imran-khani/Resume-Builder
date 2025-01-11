d6372172d8ec355d4a2e962ab48d9f72acdfdfb3
imran khan <imranloveh01@gmail.com>
2025-01-12 01:26:24 +0500

    add ResumeLayout component and update routing structure for Resume page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import RootLayout from "./Layout/RootLayout";
 import Resume from "./pages/Resume";
+import ResumeLayout from "./Layout/ResumeLayout";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<RootLayout />}>
         <Route index element={<Home />} />
-      <Route path="/resume" element={<Resume />} />
+        <Route path="resume" element={<ResumeLayout />}>
+          <Route index element={<Resume />} />
+        </Route>
       </Route>
     </Routes>
   );

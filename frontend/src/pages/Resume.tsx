ccfa13ecff1792c05fbb041bf6d79ed56a1458f0
imran khan <imranloveh01@gmail.com>
2025-01-12 02:15:38 +0500

    refactor Resume component for improved readability and formatting

--- frontend/src/pages/Resume.tsx
+++ frontend/src/pages/Resume.tsx
@@ -6,9 +6,14 @@ import ResumeContent from "../components/ResumeContent";
 const Resume = () => {
   const [currentStep, setCurrentStep] = useState(0);
 
-
-  const steps = [ "Personal Information", "Education", "Experience", "Skills", "Projects", "Summary"];
-  
+  const steps = [
+    "Personal Information",
+    "Education",
+    "Experience",
+    "Skills",
+    "Projects",
+    "Summary",
+  ];
 
   const resumeData = {
     personalInformation: {
@@ -45,25 +50,24 @@ const Resume = () => {
   return (
     <div className="flex flex-col space-y-5 items-center justify-center">
       <div className="p-6 flex gap-x-5">
-      {
-        steps.map((_,idx)=>(
-          <div key={idx} className={cn(
-            "border-gray-300 bg-indigo-100 items-center justify-center rounded-full h-16 w-16 flex flex-row",
-            currentStep === idx && 'bg-indigo-600 text-white'
-          )}>
-              <div>{idx + 1}</div>
+        {steps.map((_, idx) => (
+          <div
+            key={idx}
+            className={cn(
+              "border-gray-300 bg-indigo-100 items-center justify-center rounded-full h-16 w-16 flex flex-row",
+              currentStep === idx && "bg-indigo-600 text-white"
+            )}
+          >
+            <div>{idx + 1}</div>
           </div>
-        ))
-      }
-    </div>
+        ))}
+      </div>
 
-    <div>
-      <ResumeContent>
-        hello
-      </ResumeContent>
+      <div>
+        <ResumeContent>hello</ResumeContent>
+      </div>
     </div>
-    </div>
-  )
+  );
 };
 
 export default Resume;

0e247c06466e9384575a2a6fb77e0e95c431a897
imran khan <imranloveh01@gmail.com>
2025-01-12 00:57:20 +0500

    implement routing with React Router and add RootLayout and Resume components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
-import Home from "./pages/Home"
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <div className="relative flex justify-center min-h-screen bg-transparent bg-[url('bg.jpg')] bg-no-repeat bg-cover">
       <div className="absolute inset-0 bg-black/10 backdrop-blur-sm"></div>
-      <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

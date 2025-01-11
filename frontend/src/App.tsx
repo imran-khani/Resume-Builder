97a6188362713f0a22109280f5d73b2429f4c3b4
imran khan <imranloveh01@gmail.com>
2025-01-12 00:20:01 +0500

    update styling for App and Home components; enhance background and text colors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,8 @@ import Home from "./pages/Home"
 
 const App = () => {
   return (
-    <div className="flex justify-center min-h-screen bg-indigo-100">
+    <div className="relative flex justify-center min-h-screen bg-transparent bg-[url('bg.jpg')] bg-no-repeat bg-cover">
+      <div className="absolute inset-0 bg-black/10 backdrop-blur-sm"></div>
       <Home />
     </div>
   )
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,34 +3,32 @@ import Button from "../components/Button";
 
 const Home = () => {
   return (
-    <div className="py-32 flex flex-col container">
+    <div className="py-32 flex flex-col container z-50">
       <section className="flex flex-col gap-y-5 items-center">
-        <h1 className="text-indigo-500 text-5xl font-bold text-center">
+        <h1 className="text-[#333] text-5xl font-bold text-center">
           Resume Builder
         </h1>
-        <p className="max-w-xl text-xl text-center text-gray-600">
+        <p className="max-w-xl text-xl text-center text-[#333">
           Create a resume that stands out. Create a professional resume in
           minutes. Make a great first impression and get the job you want.
         </p>
-        <Button label="Get Started" size="lg" />
+        <Button className="bg-[#333]" label="Get Started" size="lg" />
       </section>
 
       <section className="py-24">
-        <h2 className="text-3xl text-center text-indigo-600 font-bold">
+        <h2 className="text-3xl text-center text-white font-bold">
           Just Three steps
         </h2>
 
-        <div className="grid grid-cols-3 my-5 gap-5">
+        <div className="grid grid-cols-3 my-9 gap-5">
           {cardsData.map((card, idx) => (
             <div
               key={idx}
-              className="shadow-md bg-white px-4 py-8 rounded-md flex flex-col items-center"
+              className="shadow-md bg-[#333] px-4 py-8 rounded-md flex flex-col items-center"
             >
-              <span className="text-indigo-500 mb-5">{card.icon}</span>
-              <h3 className="text-lg text-indigo-600 font-bold">
-                {card.title}
-              </h3>
-              <p className="">{card.description}</p>
+              <span className="text-white mb-5">{card.icon}</span>
+              <h3 className="text-lg text-white font-bold">{card.title}</h3>
+              <p className="text-white">{card.description}</p>
             </div>
           ))}
         </div>

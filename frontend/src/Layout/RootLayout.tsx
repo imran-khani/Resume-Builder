d226a26262e23adc29d53eb982f545f55a4a6848
imran khan <imranloveh01@gmail.com>
2025-01-12 01:08:34 +0500

    enhance RootLayout styling by adding padding to the container

--- frontend/src/Layout/RootLayout.tsx
+++ frontend/src/Layout/RootLayout.tsx
@@ -5,7 +5,7 @@ const RootLayout = () => {
     <div className="relative flex justify-center min-h-screen bg-transparent bg-[url('bg.jpg')] bg-no-repeat bg-cover">
       <div className="absolute inset-0 bg-black/10 backdrop-blur-sm"></div>
       {/* Wrap Outlet with a z-index container */}
-      <div className="relative z-10 w-full">
+      <div className="relative z-10 w-full container py-24">
         <Outlet />
       </div>
     </div>
